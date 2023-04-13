8b31966027ca87bbce0ee4c2b6e06df811ce0761
manuelLanderos <108149974+manuelLanderos@users.noreply.github.com>
2023-04-13 14:34:37 -0400

    commiting changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+// import TodoList from './Components/ToDo/ToDo';
+import ToDo from './Components/ToDo/ToDo'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="background-container">
+      <div className="background-animation">
+        <header className="App-header">
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <ToDo />
+        </header>
+      </div>
     </div>
   );
 }
---
+++ src/Components/ToDo/ToDo.js
@@ -0,0 +1,80 @@
+// import { Component, useState } from "react";
+import React, { useState } from 'react';
+import './ToDo.css'
+
+function TodoList() {
+  const [todos, setTodos] = useState([
+    { name: "Laundry", completed: false },
+    { name: "Wash Dishes", completed: false },
+    { name: "Vacuum", completed: false },
+
+  ])
+  function handleClick(index) {
+    const newTodos = [...todos]
+    newTodos[index].completed = !newTodos[index].completed
+    setTodos(newTodos)
+  }
+  function handleAddTodoSubmit(event) {
+    event.preventDefault()
+    const newTodoName = event.target.elements.todoName.value
+    const newTodo = { name: newTodoName, completed: false }
+    setTodos([...todos, newTodo])
+    event.target.reset()
+  }
+  function handleDeleteTodo(index) {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  }
+
+
+
+
+  return (
+    <div>
+
+      <h1>Todo List</h1>
+      <div className='list'>
+        {/* <ul> */}
+        {todos.map((todo, index) => (
+          <ul key={index} >
+            <li>
+
+              <input className='checkbox'
+                type="checkbox"
+                checked={todo.completed}
+                onChange={() => handleClick(index)}
+              />
+              <span style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
+                {todo.name}
+              </span>
+              <button className='delete' onClick={() => handleDeleteTodo(index)}>Delete</button>
+            </li>
+          </ul>
+        ))}
+      </div>
+
+      {/* </ul> */}
+      <br></br>
+      <br></br>
+      <div className='div'>
+
+        <form className='' onSubmit={handleAddTodoSubmit}>
+          <input type="text" name="todoName" placeholder="Enter To Do's" />
+          <br></br>
+          <br>
+          </br>
+          <button href="#" class="button">
+            <div class="button__line"></div>
+            <div class="button__line"></div>
+            <span class="button__text">ENTER</span>
+            <div class="button__drow1"></div>
+            <div class="button__drow2"></div>
+          </button>
+        </form>
+      </div>
+    </div >
+  );
+}
+
+export default TodoList;
